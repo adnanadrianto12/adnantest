afb58b14821bc94846c3b9a1894e0481f9806347
adnanadrianto12 <adnanadrianto42@gmail.com>
2024-12-12 05:28:12 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,17 @@
 <?php
-$host = 'sql213.infinityfree.com';
-$db_name = 'if0_37810105_scores';
-$username = 'if0_37810105';
-$password = 'DyJJBW6jyRbG';
+$host = 'sql213.infinityfree.com'; // Host database di InfinityFree
+$db_name = 'if0_37810105_scores'; // Nama database Anda
+$username = 'if0_37810105'; // Username MySQL
+$password = 'DyJJBW6jyRbG'; // Password MySQL
 
+// Menggunakan PDO untuk menghubungkan ke database MySQL
 try {
     $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
-    // Set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully"; 
 }
 catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
+
 ?>
